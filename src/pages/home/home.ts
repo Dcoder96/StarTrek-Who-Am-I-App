44feda4a8cc7f5a9222f26859342c5a808fb12ab
hdk <amir.madi34@gmail.com>
2018-10-20 21:21:21 +0200

    added color on user response

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,21 +9,30 @@ import { JeuxPage } from '../jeux/jeux';
     trigger('flipState', [
       state('active', style({
         transform: 'rotateY(179deg)'
+         
       })),
       state('inactive', style({
         transform: 'rotateY(0)'
       })),
-      transition('active => inactive', animate('500ms ease-out')),
-      transition('inactive => active', animate('500ms ease-in'))
+      transition('active => inactive', animate('1000ms ease-out')),
+      transition('inactive => active', animate('1000ms ease-in'))
     ])
   ]
 })
 export class HomePage {
+
+  myStyles = {
+ 
+    'background-color': '',
+    }
+
   flip: string = 'inactive';
   answer:boolean = false;
   toggleFlip() {
     this.flip = (this.flip == 'inactive') ? 'active' : 'inactive';
     this.answer = true;
+    this.myStyles["background-color"] = (this.myStyles["background-color"] == '' && this.answer == true) ? 'green' : '';
+    // this.myStyles["background-color"] = (this.myStyles["background-color"] == '' && this.answer == false) ? 'red' : '';
   }
   constructor(public navCtrl: NavController) {
     
